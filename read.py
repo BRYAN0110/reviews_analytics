41a410b90247f16d86b474d4ede662f03d522b14
Bryan Chen <bryan850110@gmail.com>
2021-07-21 22:55:44 +0800

    final edition

--- read.py
+++ read.py
@@ -1,8 +1,12 @@
 data = []
 count = 0
+sum_len = 0
 with open('reviews.txt', 'r') as f :
 	for line in f :
 		data.append(line)
+		sum_len = sum_len + len(data[count])
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+print(sum_len)	 
+average_len = sum_len / len(data)
+print(average_len)
+		
